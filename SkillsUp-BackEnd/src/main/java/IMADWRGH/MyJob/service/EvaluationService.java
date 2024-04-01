a0e05116f3809b4a2d493a9463f478235d2c3fe3
Imad Asserne <imad.asserne@gmail.com>
2024-04-01 10:30:43 +0000

    add method for service "Evaluation"

--- SkillsUp-BackEnd/src/main/java/IMADWRGH/MyJob/service/EvaluationService.java
+++ SkillsUp-BackEnd/src/main/java/IMADWRGH/MyJob/service/EvaluationService.java
@@ -2,7 +2,10 @@ package IMADWRGH.MyJob.service;
 
 import IMADWRGH.MyJob.Repository.EvaluationRepository;
 import IMADWRGH.MyJob.entity.Evaluation;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EvaluationService {
@@ -14,5 +17,18 @@ public class EvaluationService {
 
     public Evaluation create(Evaluation evaluation){
         return evaluationRepository.save(evaluation);
+    }
+
+    public Boolean delete(int id){
+        evaluationRepository.deleteById(id);
+        return Boolean.TRUE;
+    }
+
+    public Evaluation update(Evaluation evaluation){
+        return evaluationRepository.save(evaluation);
+    }
+
+    public List<Evaluation> list(int limit){
+        return evaluationRepository.findAll(PageRequest.of(0,limit)).toList();
     }
 }
